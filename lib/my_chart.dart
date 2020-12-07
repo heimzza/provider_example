2bc2cfce377b263b1d0b8b72183dd392331824b1
heimzza <hamzassm18@gmail.com>
2020-12-07 15:39:33 +0300

    fixed errors

--- lib/my_chart.dart
+++ lib/my_chart.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'my_schedule.dart';
+
 class MyChart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Consumer(
-      builder: (context, schedule, _) => Text(schedule.stateManagementTime),
+    return Consumer<MySchedule>(
+      builder: (context, schedule, _) => Text(schedule.stateManagementTime.toString()),
     );
   }
 }
